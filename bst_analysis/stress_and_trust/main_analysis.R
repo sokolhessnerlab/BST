2a5036fb76252280d83fcb869f77e4d8ea8564b5
Simon Haats <Simon@186-126.citrix-user.du.edu>
2021-03-15 19:06:49 -0600

    Added analysis related files

---
+++ bst_analysis/stress_and_trust/main_analysis.R
@@ -0,0 +1,218 @@
+
+#loads up the dataframes from setupData, also loads the plyr library (note this in case you use dply)
+#NOTE: this assumes that you are connected to the shlab drive, and it is attached under /Volumes
+#if this is not the case go to the top of setupData and change the path there
+source("./bst_analysis/stress_and_trust/setup_data.R")
+
+library(ggplot2)
+library(lme4)
+library(nlme)
+
+#Base Level Analysis#
+#Stress:
+#Acute
+
+#descriptives
+mean(bst_bath$stressPleasantnessRating)
+sd(bst_bath$stressPleasantnessRating)
+mean(bst_bath$controlPleasantnessRating)
+sd(bst_bath$controlPleasantnessRating)
+
+#effect of stressor on ratings
+#paired sample t-test (ie a test of differences)
+t.test(bst_bath$stressPleasantnessRating - bst_bath$controlPleasantnessRating, mu = 0, var.equal = TRUE)
+#Key Results (p < .001, mean difference = 4.1) - the ratings are statiscally very probably different
+
+#checking for a day effect on ratings
+t.test(bst_bath$diffPleasantnessRating ~ bst_bath$day2StressedBool)
+# t.test(bst_bath$stressPleasantnessRating ~ bst_bath$day2StressedBool)
+# t.test(bst_bath$controlPleasantnessRating ~ bst_bath$day2StressedBool)
+
+#Chronic
+#descriptives of PSS
+mean(bst_pss$pssSum)
+sd(bst_pss$pssSum)
+range(bst_pss$pssSum)
+count(bst_pss$pssSumCategorical)
+
+#histogram of PSS scores
+ggplot(bst_pss, aes(x=pssSum)) +
+  geom_histogram(binwidth = 1, color = "black", fill = "white") +
+  scale_x_continuous(name = "Sum", breaks = c(0,5,10,15,20,25,30,35,40), limits = c(0,40)) +
+  labs(title="Sum of PSS Scores Histogram") +
+  theme_classic()
+
+#effect of acute stressor n chronic stressor
+#t-test on if there was a difference in PSS scores based on what type of bath was recieved on day 2 (the PSS was only administered on day 2)
+t.test(bst_bath_pss$pssSum ~ bst_bath_pss$day2StressedBool)
+#Key results (p = .94, t=-.075) recieved cold bath mean = 16, didn't recieved lukewarm bath mean = 15.86 - the PSS scores are statistically not probably different
+
+#Trust:
+#Trust Rating
+#descriptives
+mean(bst_tr$rating)
+sd(bst_tr$rating)
+
+#prelim looking at different effect
+#underwent stressor or not
+by(data = bst_tr$rating, INDICES = bst_tr$stressedBool, FUN = mean)
+by(data = bst_tr$rating, INDICES = bst_tr$stressedBool, FUN = sd)
+#day
+by(data = bst_tr$rating, INDICES = bst_tr$day, FUN = mean)
+by(data = bst_tr$rating, INDICES = bst_tr$day, FUN = sd)
+#task order
+by(data = bst_tr$rating, INDICES = bst_tr$taskOrder, FUN = mean)
+by(data = bst_tr$rating, INDICES = bst_tr$taskOrder, FUN = sd)
+#what day they got the stressor on (0 means they got on day 1, 1 means they got it on day 2)
+by(data = bst_tr$rating, INDICES = bst_tr$day2StressedBool, FUN = mean)
+by(data = bst_tr$rating, INDICES = bst_tr$day2StressedBool, FUN = sd)
+#task order had a means difference of ~.3, maybe something there (sd was about 1.85)
+#day of stressor also has something there (maybe) with a means difference of .5 and a difference of ~.28 in the sd as well
+#following up on whether these possible confounds had any effect task order
+#note I include subjectID as a random effect, because without it, task order becomes close to a proxy for individual differences
+tr_confound_mod <- lme(rating ~ 1 + taskOrder + day + day2StressedBool, random = ~ 1 | subjectID, data = bst_tr_pss)
+summary(tr_confound_mod)
+#results: while none of these were techinically significant, task order was trending (p=.058). That said the effect size was pretty small (~-.08)
+#Trust Game
+#descriptives
+mean(bst_tg$shared)
+sd(bst_tg$shared)
+
+#prelim looking at different effect
+#underwent stressor
+by(data = bst_tg$shared, INDICES = bst_tg$stressedBool, FUN = mean)
+by(data = bst_tg$shared, INDICES = bst_tg$stressedBool, FUN = sd)
+#day
+by(data = bst_tg$shared, INDICES = bst_tg$day, FUN = mean)
+by(data = bst_tg$shared, INDICES = bst_tg$day, FUN = sd)
+#task order
+by(data = bst_tg$shared, INDICES = bst_tg$taskOrder, FUN = mean)
+by(data = bst_tg$shared, INDICES = bst_tg$taskOrder, FUN = sd)
+#what day they got the stressor on (0 means they got on day 1, 1 means they got it on day 2)
+by(data = bst_tg$shared, INDICES = bst_tg$day2StressedBool, FUN = mean)
+by(data = bst_tg$shared, INDICES = bst_tg$day2StressedBool, FUN = sd)
+#day had a means difference of ~.15, but again task order had a difference of ~.45 and a sd difference of ~.16 (all the other sd was ~1.57)
+#day of stressor also had a means difference of ~.22, and a sd difference of ~.13
+#note: like before I include the random intercepts of subject to accoutn for task order being a sort of proxy for that
+tg_confound_mod <- lme(shared ~ 1 + taskOrder + day + day2StressedBool, random = ~ 1 | subjectID, data = bst_tg)
+summary(tg_confound_mod)
+#Results: something interesting here, task order and stressor order weren't significant but day was, with an effect size of ~.14
+#sequential effects
+tg_seq_mod0 <- lm(shared ~ 1 + prevTrialShared, data = bst_tg)
+tg_seq_mod1 <- lm(shared ~ 1 + prevTrialFeedback, data = bst_tg)
+summary(tg_seq_mod0)
+summary(tg_seq_mod1)
+#Results: both of these models showed significance for the previous trial, notably people shared ~.32 more following a loss
+
+#the next regression looks at whether stress had anything to do with it, so feel free to uncomment the following lines to change the coding of the "stressedBool" variable
+# bst_tg$stressedBool <- as.numeric(bst_tg$stressedBool)
+# bst_tg$stressedBool <- ifelse(bst_tg$stressedBool == 0, -1, 1)
+tg_seq_mod2 <- lm(shared ~ 1 + prevTrialFeedback:stressedBool, data = bst_tg)
+summary(tg_seq_mod2)
+#Results: previous trial feedback was ~.19, so when a participant was stressed, the effect of previous trials is actually smaller?
+
+#Var Associations#
+#stress
+#pss score and the pleasantness ratings correlations
+#normal
+cor.test(bst_bath_pss$pssSum, bst_bath_pss$diffPleasantnessRating, method = 'pearson')
+cor.test(bst_bath_pss$pssSum, bst_bath_pss$controlPleasantnessRating, method = 'pearson')
+cor.test(bst_bath_pss$pssSum, bst_bath_pss$stressPleasantnessRating, method = 'pearson')
+#Results: no statistically significant correlations
+
+#as the 3 levels (also not significant)
+cor.test(bst_bath_pss$pssSumCategorical, bst_bath_pss$diffPleasantnessRating, method = 'pearson')
+
+#trust
+#on participant by participant means (TG x TR)
+#correlations:
+#on means
+cor.test(bst_t_compiled_part_avg$sharedAvg, bst_t_compiled_part_avg$ratingAvg, method = 'pearson')
+cor.test(bst_t_compiled_part_avg$sharedAvg, bst_t_compiled_part_avg$ratingAvg, method = 'spearman', exact=FALSE)
+#results: the pearson was not significant but trending (p~.08) and r~.27, the spearman was significant (p~.02) and rho~.36
+#on variances
+cor.test(bst_t_compiled_part_avg$sharedSD, bst_t_compiled_part_avg$ratingSD, method = 'pearson')
+cor.test(bst_t_compiled_part_avg$sharedSD, bst_t_compiled_part_avg$ratingSD, method = 'spearman', exact = FALSE)
+#results: no significant correlation here on standard deviations
+
+#plot of correlation
+ggplot(data = bst_t_compiled_part_avg, aes(x = sharedAvg, y = ratingAvg)) +
+  geom_point(size = 2) +
+  scale_x_continuous(name = "Shared Amount Average ($)") +
+  scale_y_continuous(name = "Trust Rating Average", limits = c(1,9), breaks = c(1,3,6,9)) +
+  geom_smooth(method = lm, se = FALSE) +
+  geom_errorbarh(aes(xmin = sharedLowSE, xmax = sharedHighSE)) +
+  geom_errorbar(aes(ymin = ratingLowSE, ymax = ratingHighSE)) +
+  theme_classic()
+
+#seeing if the correlations changed when seperating out by stressed or not condition as well
+#on means
+with(data = (subset(bst_t_compiled_part_avg_stress, stressedBool == 1)), cor.test(sharedAvg, ratingAvg, method = 'pearson')) #stressed
+with(data = (subset(bst_t_compiled_part_avg_stress, stressedBool == 0)), cor.test(sharedAvg, ratingAvg, method = 'pearson')) #not stressed
+#Results, the not stressed was trending (p = .051) r = .31, the stressed was not significant (p~.15), r = .23
+
+#variances
+with(data = (subset(bst_t_compiled_part_avg_stress, stressedBool == 1)), cor.test(sharedSD, ratingSD, method = 'pearson')) #stressed
+with(data = (subset(bst_t_compiled_part_avg_stress, stressedBool == 0)), cor.test(sharedSD, ratingSD, method = 'pearson')) #not stressed
+#Results: neither here was significant
+
+#stress x trust
+#on participant level means
+#correlations:
+#trust rating
+cor.test(bst_t_compiled_part_avg_pss$ratingAvg, bst_t_compiled_part_avg_pss$pssSum, method = 'pearson')
+cor.test(bst_t_compiled_part_avg_pss$ratingAvg, bst_t_compiled_part_avg_pss$pssSum, method = 'spearman', exact = FALSE)
+#Results: neither was statistically significant
+
+#trust game
+cor.test(bst_t_compiled_part_avg_pss$sharedAvg, bst_t_compiled_part_avg_pss$pssSum, method = 'pearson')
+cor.test(bst_t_compiled_part_avg_pss$sharedAvg, bst_t_compiled_part_avg_pss$pssSum, method = 'spearman', exact = FALSE)
+#Results: neither here was statistically significant
+
+
+#Main Main Analysis#
+#Trust Rating:
+#basic modeling of stress and trust
+tr_mod_acuteCat_chronicCont <- lme(rating~ 1 + stressedBool * pssSum, random = ~ 1 | subjectID, data = bst_tr_pss) #categorical stress
+tr_mod_acuteRat_chronciCont <- lme(rating ~ 1 + diffPleasantnessRating * pssSum, random = ~ 1 | subjectID, data = bst_tr_pss) #pleasantness ratings
+tr_mod_acuteCat_chronicCat <- lme(rating ~ 1 + stressedBool * pssSumCategorical, random = ~ 1 | subjectID, data = bst_tr_pss) #categorical PSS scores
+
+summary(tr_mod_acuteCat_chronicCont)
+summary(tr_mod_acuteRat_chronciCont)
+summary(tr_mod_acuteCat_chronicCat)
+#Results: both precitors and their interaction were not sifnificant (aside from the intercept),
+#however, using the categorical stress, the interaction of stress and pss score was p~.06, although the effect size was pretty small all things considered (~-.01)
+#Finally, using the categorical pss, the the stressed category and the interaction of the acute and chronic stressor were signficant, with an effect size of -.23 for the interaction
+#including task order because that appeared to maybe have an effect in prelim analysis
+tr_mod_cat1 <- lme(rating ~ 1 + taskOrder + stressedBool * pssSum, random = ~ 1 | subjectID, data = bst_tr_pss)
+
+summary(tr_mod_cat1)
+#Results: main effect thing to note here was that task order was not signficant (p=.075), howeverthe stressed:pss interaction did move to significance
+
+#Trust Game:
+#basic modelling of stress and trust
+tg_mod_acuteCat_chronicCont <- lme(shared ~ 1 + stressedBool * pssSum, random = ~ 1 | subjectID, data = bst_tg_pss) #categorical stress
+tg_mod_acuteRat_chronciCont <- lme(shared ~ 1 + diffPleasantnessRating * pssSum, random = ~ 1 | subjectID, data = bst_tg_pss) #pleasantness ratings
+tg_mod_acuteCat_chronicCat <- lme(shared ~ 1 + stressedBool * pssSumCategorical, random = ~ 1 | subjectID, data = bst_tg_pss) #categorical PSS scores
+
+summary(tg_mod_acuteCat_chronicCont)
+summary(tg_mod_acuteRat_chronciCont)
+summary(tg_mod_acuteCat_chronicCat)
+#Results: like before the pleasantness ratings did not accont for anything significant
+#however, the categorical stressed or not did have some interesting effects, it itself was signficant (and an effect of .32), and the interaction with PSS was significant (with an effect of -.25)
+#also the continuous PSS scores were also not signficant
+#including day because in the prelim analysis that was significant
+tg_mod_acuteCat_chronicCont_day <- lme(shared ~ 1 + day + stressedBool * pssSum, random = ~ 1 | subjectID, data = bst_tg_pss)
+
+summary(tg_mod_acuteCat_chronicCont_day)
+#Results: here it was interesting to note that the findings didn't change a ton from the previous model, however day was significant with an effect size of .073
+
+#modeling looking at gambler's fallacy/previous trial results (using the categorical stress)
+tg_mod_pt_acuteCat_chronicCont <- lme(shared ~ 1 + prevTrialFeedback + prevTrialFeedback:stressedBool + stressedBool * pssSum, random = ~ 1 | subjectID, data = bst_tg_pss) #I include the interaction term here with stressed as that was one of the main findings of FeldmannHall
+tg_mod_pt_acuteCat_chronicCont_day <- lme(shared ~ 1 + day +prevTrialFeedback + prevTrialFeedback:stressedBool + stressedBool * pssSum, random = ~ 1 | subjectID, data = bst_tg_pss) #including the possible confound of day
+
+summary(tg_mod_pt_acuteCat_chronicCont)
+summary(tg_mod_pt_acuteCat_chronicCont_day)
+#Results: interesting here that there didn't really seem to be an effect of the outcome of a previous trial, and the patterns seen elsewhere held true
+#day, stressed, and pss:stressed were all signficant, with the interaction having the largest effect size when taking into account the min to max possibilities
+
---
+++ bst_analysis/stress_and_trust/setup_data.R
@@ -0,0 +1,207 @@
+#-loads up a series of data frames of the stress constructs and trust constructs for BST for later analysis
+#-aggregates data into additional data frames
+#-performs coding and computation of variables
+
+curwd <- getwd()
+setwd("/Volumes/shlab/Projects/BST/data/Current_Dataframes") #set this appropriately, this should work if you're on mac/connected to shlab drive, windows will have a different path
+
+#loads up the necessary libraries
+library(plyr)
+
+#Color Blind Palette for graphing
+cbPalette <- c("#999999", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
+
+##Start Data Retreival, Cleaning, Formatting: ##
+
+#setting Up Stress DFs:#
+
+#PSS:
+bst_pss <- read.csv("./SurveyData/PSSData_noVarDescrip.csv") #reads in the .csv
+#bst_pss <- bst_pss[-c(1),] #removes the first row which just has variable descriptions
+names(bst_pss)[names(bst_pss) == "subjectNumber"] <- 'subjectID' #renames the subject identification column to maintain consistency
+
+#recode reverse coded values for the 4 questions that need it
+bst_pss$personalProblemConfidenceRecode <- 4 - bst_pss$personalProblemConfidence
+bst_pss$goingYourWayRecode <- 4 - (bst_pss$goingYourWay)
+bst_pss$irritationControlRecode <- 4 - bst_pss$irritationControl
+bst_pss$onTopOfThingsRecode <- 4 - bst_pss$onTopOfThings
+#calculate and check the sum of all the parts of the pss (the pss score)
+bst_pss$pssSum <- (bst_pss$unexpectedUpset + bst_pss$unableControl + bst_pss$nervous + bst_pss$personalProblemConfidenceRecode + bst_pss$goingYourWayRecode + bst_pss$couldNotCope + bst_pss$irritationControlRecode + bst_pss$onTopOfThingsRecode + bst_pss$outsideOfControl + bst_pss$diffPilingUp)
+
+bst_pss$pssSumCategorical <- ifelse(bst_pss$pssSum <= 13, 0,
+                                    ifelse(bst_pss$pssSum >= 27, 2, 1)) #calculates the pss score in the 3 level categorical version
+
+#checking the categorical math worked out:
+# count(bst_pss$pssSum)
+# count(bst_pss$pssSumcategorical)
+
+#Bath Ratings:
+bst_bathPleasantness <- read.csv("bathPleasantness.csv") #reads in the pleasantness ratings
+bst_bathOrder <- read.csv("bathOrder.csv") #reads in the bath ordering
+names(bst_bathOrder)[names(bst_bathOrder) == "BST.."] <- 'subjectID'
+
+bst_bath <- merge(bst_bathPleasantness, bst_bathOrder, by = "subjectID") #merges the two bath DF's into a single one
+
+bst_bath$diffPleasantnessRating<- (bst_bath$STRESS - bst_bath$CONTROL) #calculates the rating difference between the stress and control ratings
+
+bst_bath$day2StressedBool <- ifelse(bst_bath$Day.2 == "CONTROL", 0, 1) #calculates a boolean for whether a participant was stressed on the second day or if they recieved the control
+
+#double checks the math worked out:
+# count(bst_bath$Day.2) #prints out how many recieved the control on day 2 and how many recieved the stressor on day (in string format)
+# count(bst_bath$day2Stressed) #prints out how many recieved the control on day 2 and how many recieved the stressor on day 2 (in numerical format)
+
+#renaming columns for clarity
+names(bst_bath)[names(bst_bath) == "CONTROL"] <- "controlPleasantnessRating"
+names(bst_bath)[names(bst_bath) == "STRESS"] <- "stressPleasantnessRating"
+names(bst_bath)[names(bst_bath) == "Day.1"] <- "bathRecievedDay1"
+names(bst_bath)[names(bst_bath) == "Day.2"] <- "bathRecievedDay2"
+
+#combining acute and chronic stressors
+bst_bath_pss <- merge(bst_bath, bst_pss, by = "subjectID")
+
+#Setting Up Trust DFs:#
+#Trust Game:
+bst_tg <- read.csv("allTGData_20200626.csv") #reads in trust game data
+
+#renaming columns for clarity
+names(bst_tg)[names(bst_tg) == "condition"] <- "taskOrder"
+names(bst_tg)[names(bst_tg) == "RT"] <- "responseTime"
+
+#combines the trust game data with the bath data for calculation of variables and later analysis
+bst_tg <- merge(bst_tg, bst_bath, by = "subjectID")
+
+#calculating new factors for whether a participant was stressed or not before doing the trust task
+bst_tg$stressedBool <- ifelse((bst_tg$day2StressedBool == 1 & bst_tg$day == 1), 0,
+                              ifelse((bst_tg$day2StressedBool == 0 & bst_tg$day == 1), 1,
+                                     ifelse((bst_tg$day2StressedBool == 1 & bst_tg$day == 2), 1,
+                                            ifelse((bst_tg$day2StressedBool == 0 & bst_tg$day == 2), 0, NA))))
+#double checking the math above worked out (there should be no NA)
+# count(bst_tg$stressedBool)
+
+#calculating results of previous trials
+bst_tg$prevTrialShared <- 0 #sets up as 'neutral condition'
+bst_tg$prevTrialFeedback <- 0 #sets up as 'neutral condition'
+#calculates what the previous trial was:
+bst_tg <- bst_tg[order(bst_tg$subjectID, bst_tg$day, bst_tg$cumTrialNum),] #orders dataset by cumulative trial (within a day and subjectID)
+for(i in 1:nrow(bst_tg)) #iterates through the now ordered dataset
+{
+  if(bst_tg[i, "cumTrialNum"] != 1) #if its the first trial, then there is nothing to change, as there was no 'previous trial'
+  {
+    if(bst_tg[(i-1), "shared"] >= 1) #if they shared any money on the previous trial
+    {
+      bst_tg[i, "prevTrialShared"] <- 1 #then the current trials prev trial shared amount can be set to 1 (ie they did share on the last trial)
+      if(bst_tg[(i-1), "received"] == 0) #of they didn't recieve any money back, the feedback is set to -1 (ie on the last trial they lost the money they shared)
+      {
+        bst_tg[i, "prevTrialFeedback"] <- -1
+      }
+      else #if they did recieve money back after sharing, their feedback is set to 1
+      {
+        bst_tg[i, "prevTrialFeedback"] <- 1
+      }
+    }
+    else #if they didn't share any money, the shared amount is set so, and the feedback isn't changed
+    {
+      bst_tg[i, "prevTrialShared"] <- -1
+    }
+  }
+}
+#make sure the counts are where they should be (the coding worked right)
+# count(bst_tg$shared)
+# count(bst_tg$prevTrialShared)
+# count(bst_tg$partnerChoice)
+# count(bst_tg$prevTrialFeedback)
+
+#setting up a categorical for if a participant shared any money at all
+bst_tg$sharedBool <- ifelse(bst_tg$shared == 0, 0, 1)
+
+#because merging with the PSS will remove 3 participants, this is a sperate DF for only those participants that have a PSS score
+bst_tg_pss <- merge(bst_pss, bst_tg, by = "subjectID") #merges with the pss for later analysis
+
+#Trust Rating:
+bst_tr <- read.csv("allTRData_20200626.csv")
+
+#renaming columns for clarity
+names(bst_tr)[names(bst_tr) == "response"] <- "rating"
+names(bst_tr)[names(bst_tr) == "condition"] <- "taskOrder"
+names(bst_tr)[names(bst_tr) == "RT"] <- "responseTime"
+
+#combines the trust rating data with the bath data for calculation of variables and later analysis
+bst_tr <- merge(bst_tr, bst_bath, by = "subjectID")
+
+#calculating new factors for whether a participant was stressed or not before doing the trust task
+bst_tr$stressedBool <- ifelse((bst_tr$day2StressedBool == 1 & bst_tr$day == 1), 0,
+                              ifelse((bst_tr$day2StressedBool == 0 & bst_tr$day == 1), 1,
+                                     ifelse((bst_tr$day2StressedBool == 1 & bst_tr$day == 2), 1,
+                                            ifelse((bst_tr$day2StressedBool == 0 & bst_tr$day == 2), 0, NA))))
+#double checking the math above worked out (there should be no NA)
+count(bst_tg$stressedBool)
+
+#because merging with the PSS will remove 3 participants, this is a sperate DF for only those participants that have a PSS score
+bst_tr_pss <- merge(bst_pss, bst_tr, by = "subjectID") #for use when looking at pss x trust rating
+
+
+#setting up aggregated DFs:
+#overall:
+#for the trust game:
+bst_tg_part_avg <- aggregate(shared ~ subjectID, data = bst_tg, FUN = mean) #aggregates the trust game by participant
+names(bst_tg_part_avg)[names(bst_tg_part_avg) == "shared"] <- "sharedAvg"
+bst_tg_part_avg$sharedSD <- (aggregate(shared ~ subjectID, data =bst_tg, FUN = sd))$shared #calculates each participants standard deviation shared amount
+bst_tg_part_avg$sharedSE <- (bst_tg_part_avg$sharedSD / sqrt(nrow(bst_tg_part_avg))) #calculates the standard error
+bst_tg_part_avg$sharedHighSE <- bst_tg_part_avg$sharedAvg +  bst_tg_part_avg$sharedSE #calculates the standard error high bound
+bst_tg_part_avg$sharedLowSE <- bst_tg_part_avg$sharedAvg - bst_tg_part_avg$sharedSE #calculates the standard error low bound
+
+#for the trust rating
+bst_tr_part_avg <- aggregate(rating ~ subjectID, data = bst_tr, FUN = mean) #aggregates the trust rating task for each participant
+names(bst_tr_part_avg)[names(bst_tr_part_avg) == "rating"] <- "ratingAvg"
+bst_tr_part_avg$ratingSD <- aggregate(rating ~ subjectID, data = bst_tr, FUN = mean)$rating #calculates each particpant's standard deviation shared amount
+bst_tr_part_avg$ratingSE <- (bst_tr_part_avg$ratingSD / sqrt(nrow(bst_tr_part_avg)))
+bst_tr_part_avg$ratingHighSE <- bst_tr_part_avg$ratingAvg + bst_tr_part_avg$ratingSE #calculates the standard error high bound
+bst_tr_part_avg$ratingLowSE <- bst_tr_part_avg$ratingAvg - bst_tr_part_avg$ratingSE #calculates the standard error low bound
+
+#compiles all the averaged participant data into one spot
+bst_t_compiled_part_avg <- merge(bst_tg_part_avg, bst_tr_part_avg, by = 'subjectID')
+bst_t_compiled_part_avg <- merge(bst_t_compiled_part_avg, bst_bath, by = 'subjectID')
+
+bst_t_compiled_part_avg_pss <- merge(bst_t_compiled_part_avg, bst_pss, by = 'subjectID')
+
+#split up by stressed or not
+#for the trust game:
+bst_tg_part_avg_stress <- aggregate(shared ~ subjectID + stressedBool, data = bst_tg, FUN = mean) #aggregates the trust game by participant
+names(bst_tg_part_avg_stress)[names(bst_tg_part_avg_stress) == "shared"] <- "sharedAvg"
+#names(bst_tg_part_avg_stress)[names(bst_tg_part_avg_stress) == "stressedBool"] <- ""
+bst_tg_part_avg_stress$sharedSD <- (aggregate(shared ~ subjectID + stressedBool, data =bst_tg, FUN = sd))$shared #calculates each participants standard deviation shared amount
+bst_tg_part_avg_stress$sharedSE <- (bst_tg_part_avg_stress$sharedSD / sqrt(nrow(bst_tg_part_avg_stress))) #calculates the standard error
+bst_tg_part_avg_stress$sharedHighSE <- bst_tg_part_avg_stress$sharedAvg +  bst_tg_part_avg_stress$sharedSE #calculates the standard error high bound
+bst_tg_part_avg_stress$sharedLowSE <- bst_tg_part_avg_stress$sharedAvg - bst_tg_part_avg_stress$sharedSE #calculates the standard error low bound
+
+#for the trust rating
+bst_tr_part_avg_stress <- aggregate(rating ~ subjectID + stressedBool, data = bst_tr, FUN = mean) #aggregates the trust rating task for each participant
+names(bst_tr_part_avg_stress)[names(bst_tr_part_avg_stress) == "rating"] <- "ratingAvg"
+bst_tr_part_avg_stress$ratingSD <- aggregate(rating ~ subjectID + stressedBool, data = bst_tr, FUN = mean)$rating #calculates each particpant's standard deviation shared amount
+bst_tr_part_avg_stress$ratingSE <- (bst_tr_part_avg_stress$ratingSD / sqrt(nrow(bst_tr_part_avg_stress)))
+bst_tr_part_avg_stress$ratingHighSE <- bst_tr_part_avg_stress$ratingAvg + bst_tr_part_avg_stress$ratingSE #calculates the standard error high bound
+bst_tr_part_avg_stress$ratingLowSE <- bst_tr_part_avg_stress$ratingAvg - bst_tr_part_avg_stress$ratingSE #calculates the standard error low bound
+
+#compiles all the averaged participant data into one spot
+bst_t_compiled_part_avg_stress <- merge(bst_tg_part_avg_stress, bst_tr_part_avg_stress, by = c('subjectID', 'stressedBool'))
+bst_t_compiled_part_avg_stress <- merge(bst_t_compiled_part_avg_stress, bst_bath, by = 'subjectID')
+
+bst_t_compiled_part_avg_stress_pss <- merge(bst_t_compiled_part_avg_stress, bst_pss, by = 'subjectID')
+
+#cleans up the strictly unnecessary DFs and vars
+rm(bst_bathOrder)
+rm(bst_bathPleasantness)
+rm(bst_tg_part_avg)
+rm(bst_tg_part_avg_stress)
+rm(bst_tr_part_avg)
+rm(bst_tr_part_avg_stress)
+rm(i)
+
+#you could also remove these if you really feel the need, the data is contained in other DFs, but I keep them for clarity in later analysis
+#rm(bst_pss)
+#rm(bst_bath)
+#rm(bst_bath_pss)
+
+#finally, resets the working directory
+setwd(curwd)
+
