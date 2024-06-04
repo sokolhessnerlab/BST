a0201e080dd39d6063827589bfbf27989562ba9a
Peter Sokol-Hessner (he/him/his) <hessner@gmail.com>
2024-06-04 10:08:46 -0600

    Finishing up averages & variances
    Finishing up averages & variances calculations in RTs & offers, and beginning with linear regressions of time.
    
    Co-Authored-By: Evrim Baykal <evrimbaykalresearch@gmail.com>

--- bst_analysis/stress_and_trust/trust_analysis.R
+++ bst_analysis/stress_and_trust/trust_analysis.R
@@ -23,7 +23,7 @@ library(lmerTest) # adds more useful info to the output of lmer's
 #converts scientific notation to decimal
 options(scipen=999)
 
-#### TRUST RATINGS ####
+# TRUST RATINGS ################################################################################
 
 #BST trust rating for the trust perception task.
 
@@ -50,14 +50,14 @@ by(data = tmpvect$rating, INDICES = tmpvect$day, FUN = mean)
 #Day 2: 4.680925
 
 
-#### TRUST GAME ####
+# TRUST GAME ####################################################################################
 
 
 # Trust Game Task Order Effects
 #Q: What are the effects of task order on peoples' trust behaviors? 
 # (collapsed)
-by(data = trustGame$shared, INDICES = trustGame$taskOrder, FUN = mean)
-by(data = trustGame$shared, INDICES = trustGame$taskOrder, FUN = sd)
+by(data = trustGame$shared, INDICES = trustGame$taskOrder, FUN = mean, na.rm = T)
+by(data = trustGame$shared, INDICES = trustGame$taskOrder, FUN = sd, na.rm = T)
 # On average, people share more when the CPT (stressor) is given on day 1)
 # Task Order 1: 2.722(1.640), Task Order 2: 2.272(1.477)
 #(subject-level)
@@ -69,8 +69,8 @@ by(data = tmpvect$shared, INDICES = tmpvect$taskOrder, FUN = mean) # order 1 = 4
 
 # Q: Were participants stressed before doing the trust task?
 
-
-##### Subject-Level Loop #####
+## TG: Averages & Variances in Offers & RTs #######################################################
+### Subject-Level Loop ##########################################################################
 tg_sub_level_colnames = c(
   'tg_mean_shared',
   'tg_mean_sharedW',
@@ -120,7 +120,7 @@ for (s in 1:number_of_subjects){
 }
 
 
-##### Mean Offer Visualizations & Analysis #####
+### Mean Offer Visualizations & Analysis ############################################################
 
 # Visualize Distributions of Mean Offers
 mean_shared_hist = hist(tg_sub_level$tg_mean_shared, breaks = seq(from = 0, to = 5, by = 0.5), plot = F);
@@ -155,32 +155,10 @@ abline(a = 0, b = 1, col = 'black')
 points(x = mean(tg_sub_level$tg_mean_sharedB), y = mean(tg_sub_level$tg_mean_sharedO), pch = 18, cex = 6)
 par(mfrow = c(1,1)) # Returning graphs to plot 1 at a time
 
-# Patterns over time in sharing
-# Q: What are the effects of cumulative trials on sharing?
-fit_shared_cumTrial = lmer(shared ~ 1 + cumTrialNum + (1 | subjectID), data = trustGame)
-summary(fit_shared_cumTrial)
-# A1: with increasing trial number, people share less. 
 
-tmpdata = trustGame[1:2,]
-tmpdata$cumTrialNum = c(1, 138)
-x = predict(fit_shared_cumTrial, newdata = tmpdata)
-# A2: On the first trial, mean shared is ~ $2.246, while on the last trial it's ~ $1.985
-# Trust declines over time. 
-
-
-##### Response Time Visualizations & Analysis #####
+### Response Time Visualizations & Analysis ###########################################################
 
 # Reaction Times (decision speed)
-
-#tmp_data %>%
-  #ggplot(aes(responseTime)) +
-  #geom_density()
-
-#tmp_data %>%
-  #ggplot(aes(responseTime, fill = partnerRace_0w_1b_2o)) +
-  #geom_histogram(binwidth = 200, color = "black", alpha = .7) +
-  #geom_vline(aes(xintercept = mean(responseTime, na.rm = TRUE)), color = "red", 
-             #linetype = "dashed", size = 1)
 
 rt_shared_hist = hist(tg_sub_level$tg_rt_shared, breaks = seq(from = 0, to = 10, length.out = 12), plot = F);
 rt_sharedW_hist = hist(tg_sub_level$tg_rt_sharedW, breaks = seq(from = 0, to = 10, length.out = 12), plot = F)
@@ -201,9 +179,9 @@ legend(x = 8.25, y = .375, c('Overall','White','Black','Other'), lwd = 2, col = 
 
 
 #t-tests for Mean RTs (race comparisons)
-t.test(tg_sub_level$tg_rt_sharedW, tg_sub_level$tg_rt_sharedB, paired = T) # p = 0.9346   B = W
-t.test(tg_sub_level$tg_rt_sharedW, tg_sub_level$tg_rt_sharedO, paired = T) # p = 0.9143   O = W
-t.test(tg_sub_level$tg_rt_sharedB, tg_sub_level$tg_rt_sharedO, paired = T) # p = 0.9711   B = O
+t.test(tg_sub_level$tg_rt_sharedW, tg_sub_level$tg_rt_sharedB, paired = T) # p = 0.20   B = W
+t.test(tg_sub_level$tg_rt_sharedW, tg_sub_level$tg_rt_sharedO, paired = T) # p = 0.27   O = W
+t.test(tg_sub_level$tg_rt_sharedB, tg_sub_level$tg_rt_sharedO, paired = T) # p = 0.98   B = O
 
 
 par(mfrow = c(1,3)) # Returning graphs to plot 1 at a time
@@ -229,22 +207,14 @@ for (s in 1:number_of_subjects){
   dev.off()
 }
 
-
-# Patterns over time in RT
-# Q: What are the effects of cumulative trials on reaction times?
-fit_rt_cumTrial = lmer(responseTime ~ 1 + cumTrialNum + (1 | subjectID), data = trustGame)
-summary(fit_rt_cumTrial)
-# A1: with increasing trial number, people get significantly faster at making their trust decisions (RT decreases). 
-
-tmpdata = trustGame[1:2,]
-tmpdata$cumTrialNum = c(1, 138)
-x = predict(fit_rt_cumTrial, newdata = tmpdata)
-# A2: On the first trial, rt is 3.675 seconds, while on the last trial rt is 1.111 seconds
-# People make trust decisions much faster over time. 
-# Attribute to learning the task
+t.test(tg_sub_level$tg_rt_var_sharedW, tg_sub_level$tg_rt_var_sharedB, paired = T) # p = 0.47
+t.test(tg_sub_level$tg_rt_var_sharedW, tg_sub_level$tg_rt_var_sharedO, paired = T) # p = 0.94
+t.test(tg_sub_level$tg_rt_var_sharedB, tg_sub_level$tg_rt_var_sharedO, paired = T) # p = 0.49
+# Within-subject variance in RTs during offers are equal
 
 
-##### Variances in Offer Decisions Visualizations & Analysis #####
+
+### Variances in Offer Decisions Visualizations & Analysis #######################################
 
 # Visualize Variance in Distributions of Mean Offers
 var_shared_hist = hist(tg_sub_level$tg_var_shared, breaks = seq(from = 0, to = 7, by = 0.5), plot = F);
@@ -259,9 +229,9 @@ lines(var_sharedO_hist$mids, var_sharedO_hist$density, col = rgb(0,0,1), lwd = 3
 legend(x = 3.7, y = .6, c('Overall','White','Black','Other'), lwd = 2, col = c('black','red','green','blue'))
 
 #t-tests for Offer Variances
-t.test(tg_sub_level$tg_var_sharedW, tg_sub_level$tg_var_sharedB, paired = T) # p = 0.314      B = W
-t.test(tg_sub_level$tg_var_sharedW, tg_sub_level$tg_var_sharedO, paired = T) # p = 0.008454   O > W
-t.test(tg_sub_level$tg_var_sharedB, tg_sub_level$tg_var_sharedO, paired = T) # p = 0.1231     B = O
+t.test(tg_sub_level$tg_var_sharedW, tg_sub_level$tg_var_sharedB, paired = T) # p = 0.33      B = W
+t.test(tg_sub_level$tg_var_sharedW, tg_sub_level$tg_var_sharedO, paired = T) # p = 0.025   W > O
+t.test(tg_sub_level$tg_var_sharedB, tg_sub_level$tg_var_sharedO, paired = T) # p = 0.18     B = O
 #A: Participants vary significantly on trust for white vs. other partners, showing greater variance for other vs. white partners in shared amounts
 #A: Interestingly, this difference in variance for partner trust is not significant when comparing shares for black vs. other partners or black vs. white partners.
 #RQ: Is there a different model of sharing depending on partner race (black vs. white) and when partner is "other" race, participants are more varied in how they share?
@@ -281,7 +251,93 @@ abline(a = 0, b = 1, col = 'black')
 points(x = var(tg_sub_level$tg_var_sharedB), y = var(tg_sub_level$tg_var_sharedO), pch = 18, cex = 6)
 par(mfrow = c(1,1)) # Returning graphs to plot 1 at a time
 
+# Offers to White partners are most variable, then offers to black partners, then offers to other partners.
+# Only W>O is significant. 
+# Potentially explanations:
+#   - More individuation of white partners (and less with Other)
+#   - More learning or other adjustment during the task w/ white partners (and less with Other)
 
+# Between-Subject Variances
+between_subj_var_shared_W = var(tg_sub_level$tg_mean_sharedW);
+between_subj_var_shared_B = var(tg_sub_level$tg_mean_sharedB);
+between_subj_var_shared_O = var(tg_sub_level$tg_mean_sharedO);
+# numeric difference here (W = O > B)... 
+
+var.test(tg_sub_level$tg_mean_sharedW, tg_sub_level$tg_mean_sharedB) # p = 0.52   W = B
+var.test(tg_sub_level$tg_mean_sharedW, tg_sub_level$tg_mean_sharedO) # p = 0.97   W = O
+var.test(tg_sub_level$tg_mean_sharedB, tg_sub_level$tg_mean_sharedO) # p = 0.54   B = O
+# ... but no significant differences in variance across subjects in mean offers.
+
+
+### Takeaways: Analyses of TG Offer Amounts, RTs, and Variance #####################################
+# Offers: People offered more to B > O > W
+# RTs: No differences in mean RTs or in the variance of RTs (by race).
+# Offer Variances (within): Order is W > B > O, but only W > O is significant
+# Offer Variances (between): No differences in variance in mean offers
+#
+# TL;DR: People offer different amounts on average (B > O > W), and might be more variable
+# in their offers to white partners/less variable in their offers to other partners, 
+# but RTs suggest that similar processes might be at work here, on average.
+
+## TG: Changes over time ########################################################################
+
+# Goal: to look at how offers and RTs change over time.
+
+### Cumulative Trial Number Effects ################################################################
+
+trustGame$dayrs = trustGame$day - 1; # dayrs = 0 on FIRST day, and 1 on SECOND day; additive effect of 2nd day
+trustGame$cumTrialNumAcrossDays = trustGame$cumTrialNum + trustGame$dayrs * (max(trustGame$cumTrialNum))
+
+# Q: What are the effects of cumulative trials on OFFERS
+fit_shared_cumTrial = lmer(shared ~ 1 + cumTrialNum + dayrs + (1 | subjectID), data = trustGame)
+summary(fit_shared_cumTrial)
+# A1: Offer amounts decline within-day, but appear to have a big day 2 boost
+
+tmpdata = trustGame[1:4,]
+tmpdata$cumTrialNum = c(1, 76, 1, 76)
+# tmpdata$cumTrialNumAcrossDays = c(1, 76, 77, 152)
+tmpdata$dayrs = c(0, 0, 1, 1)
+firstlast_Offer = predict(fit_shared_cumTrial, newdata = tmpdata)
+# A2: DAY 1, Trial 1:  $2.18
+#     DAY 1, Trial 76: $2.04
+#     DAY 2, Trial 1:  $2.31
+#     Day 2, Trial 76: $2.17
+# Trust declines within session, but increases across days. 
+
+# TO DO's:
+# Model-free calculation of mean offer by window (4 windows every day; 19 trials/window).
+# Do that by race.
+# Then return to linear regressions to model the effects we see.
+
+
+
+# Patterns over time in RT
+# Q: What are the effects of cumulative trials on reaction times?
+trustGame$sqrtrt = sqrt(trustGame$responseTime) # handle skew
+
+fit_rt_cumTrial = lmer(sqrtrt ~ 1 + cumTrialNum + (1 | subjectID), data = trustGame)
+summary(fit_rt_cumTrial)
+# A1: with increasing trial number, people get significantly faster at making their trust decisions (RT decreases). 
+
+tmpdata = trustGame[1:2,]
+tmpdata$cumTrialNum = c(1, 138)
+x = predict(fit_rt_cumTrial, newdata = tmpdata)
+firstlast_RT = x^2
+# A2: On the first trial, rt is 3.675 seconds, while on the last trial rt is 1.111 seconds
+# People make trust decisions much faster over time. 
+# Attribute to learning the task
+
+#### Takeaways: Cumulative Time ################################################################
+# People share less and do so more quickly with trial. Effects are big(ish).
+# $2.25 in 3.2s -> $1.99 in 1.7s
+# (decline of $0.26 and 1.6s!)
+
+
+
+
+
+
+# NOTES BELOW HERE ################################################################################
 
 # Q: Did people trust more in black, white, other races under stress (acute/chronic stress)?
 
