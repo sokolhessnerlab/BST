4fd3e9eab1981b9649527cfd1977f357024de4b7
Evrim <evrimbaykalresearch@gmail.com>
2024-09-06 11:59:47 -0600

    Update iat_analysis.R
    Added small df name changes to the iat_analysis.R script from Daniel.

--- bst_analysis/bst_scripts/iat_analysis.R
+++ bst_analysis/bst_scripts/iat_analysis.R
@@ -1,20 +1,20 @@
 # Load in IAT_Data
 library(IATanalytics)
-df <- subset(IAT_Data[61:300,], select = c(blockNum, # first column should be Block (0-6)
+iat_processed <- subset(IAT_Data[61:300,], select = c(blockNum, # first column should be Block (0-6)
                                            trialNum, # second column should be Trial (0-19 for training blocks, 0-39 for test blocks)
                                            corrans, # third column should be the category the trial belongs to (e.g., "pleasant," dependent on your IAT
                                            stimulus, # fourth column should be type of item within that category (again, dependent on your IAT)
                                            correct, # fifth column should be a dummy variable indicating whether the participant was correct
                                            RT)) # sixth and final column should be RT in ms))
-df$RT <- df$RT*1000 # change RT from seconds to ms 
+iat_processed$RT <- iat_processed$RT*1000 # change RT from seconds to ms 
 
 # Re-code blockNum so it's sequential:
-df$blockNum[1:40] <- 0
-df$blockNum[41:80] <- 1
-df$blockNum[81:100] <- 2
-df$blockNum[101:140] <- 3
-df$blockNum[141:160] <- 4
-df$blockNum[161:180] <- 5
-df$blockNum[181:240] <- 6
+iat_processed$blockNum[1:40] <- 0
+iat_processed$blockNum[41:80] <- 1
+iat_processed$blockNum[81:100] <- 2
+iat_processed$blockNum[101:140] <- 3
+iat_processed$blockNum[141:160] <- 4
+iat_processed$blockNum[161:180] <- 5
+iat_processed$blockNum[181:240] <- 6
 
-IATanalytics(df)
+IATanalytics(iat_processed)
